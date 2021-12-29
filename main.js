f764d2c5ccf3750fae2ed987d0553da3fc7a5f60
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2021-12-29 16:32:50 +0100

    several levels

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-let finished = false;
 const keyBoardKeys = [];
 let heroPlatform = undefined;
+let scene = undefined;
+let initialPosition = undefined;
+let iLevel = 0;
 let accY = 5;
-window.onload = loop;
+
+window.onload = () => { loadLevel(); loop() };
 window.onkeydown = (evt) => { keyBoardKeys[evt.keyCode] = evt.keyCode; }
 window.onkeyup = (evt) => { keyBoardKeys[evt.keyCode] = false; }
+
+function getPositionHero() {
+    const rect = hero.getBoundingClientRect();
+    return { left: rect.left, top: rect.top, width: rect.width, height: rect.height };
+}
 
 function getPosition(obj) {
     const rect = obj.getBoundingClientRect();
@@ -12,28 +20,40 @@ function getPosition(obj) {
 }
 
 function intersect(rect1, rect2) {
-    return rect1.top + rect1.height > rect2.top && rect1.left + rect1.width > rect2.left &&
-        rect1.top < rect2.top + rect2.height && rect1.left < rect2.left + rect2.width;
+    return rect1.top + rect1.height > rect2.top && rect1.left + rect1.width > rect2.left && rect1.top < rect2.top + rect2.height && rect1.left < rect2.left + rect2.width;
 }
 
-function loop() {
-    if (keyBoardKeys[37]) hero.style.left = Math.max(0, (getPosition(hero).left - 5)) + "px";
-    if (keyBoardKeys[39]) hero.style.left = Math.min(800, (getPosition(hero).left + 5)) + "px";
-    if (keyBoardKeys[38] && heroPlatform) {
-        accY = -10; hero.style.top = getPosition(hero).top - 20 + "px";
-    }
-    accY = Math.min(5, accY + 1);
-    const platforms = document.getElementsByTagName("PLATFORM");
+function left() { hero.style.left = (getPositionHero().left - 5) + "px"; }
+function right() { hero.style.left = (getPositionHero().left + 5) + "px"; }
+
+function loadLevel() {
+    for (const scene of videogame.getElementsByTagName("LEVEL"))
+        scene.style.display = "none";
+    scene = videogame.children[iLevel];
+    scene.style.display = "";
     heroPlatform = undefined;
-    for (let i = 0; i < platforms.length; i++) {
-        const platform = platforms[i];
-        if (intersect(getPosition(hero), getPosition(platform))) {
-            heroPlatform = platform;
-            platform.classList.add("on");
-        }
+    initialPosition = { x: getPositionHero().left, y: getPositionHero().top };
+}
+
+
+
+function loop() {
+    if (keyBoardKeys[37]) left();
+    if (keyBoardKeys[39]) right();
+    if (keyBoardKeys[38] && heroPlatform) { accY = -10; hero.style.top = getPositionHero().top - 20 + "px"; }
+    accY = Math.min(5, accY + 1);
+
+    heroPlatform = undefined;
+    for (const platform of scene.getElementsByTagName("PLATFORM")) {
+        if (getComputedStyle(platform).opacity > 0.2)
+            if (intersect(getPositionHero(), getPosition(platform))) {
+                heroPlatform = platform;
+                platform.classList.add("on");
+            }
     }
-    if (!finished && intersect(getPosition(hero), getPosition(win))) { finished = true; msg.innerHTML = "you won!"; }
-    if (!finished && getPosition(hero).top > scene.offsetHeight) { msg.innerHTML = "you loose!"; finished = true; }
+    const win = scene.getElementsByTagName("win")[0];
+    if (win && intersect(getPositionHero(), getPosition(win))) { hero.style.top = parseInt(hero.style.top) - 50 + "px"; iLevel++; loadLevel(); }
+    if (getPositionHero().top > scene.offsetHeight) { hero.style.left = initialPosition.x + "px"; hero.style.top = initialPosition.y + "px"; }
     hero.style.top = (heroPlatform == undefined) ? (getPosition(hero).top + accY) + "px" :
         getPosition(heroPlatform).top - getPosition(hero).height + 1 + "px";
     window.requestAnimationFrame(loop);
